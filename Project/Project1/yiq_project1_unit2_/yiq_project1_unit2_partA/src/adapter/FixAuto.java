1788872e2ad28f1959b11e64707b4bb5d2ec8d70
smartYi <smartqiuyi@gmail.com>
2015-09-23 16:37:14 -0400

    unit2

---
+++ Project/Project1/yiq_project1_unit2_/yiq_project1_unit2_partA/src/adapter/FixAuto.java
@@ -0,0 +1,17 @@
+package adapter;
+
+/**
+ * This interface is to fix exceptions that will occur in this program. 
+ * @author qiuyi
+ *
+ */
+
+public interface FixAuto {
+	/**
+	 * This method is to fix any exceptions that will occur in this program.
+	 * This is an abstract method that needs to be exact defined in outer class.
+	 * @param errno
+	 * @return
+	 */
+	public String fix(int errno);
+}
